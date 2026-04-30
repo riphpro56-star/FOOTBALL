bcf118d01ad69de16ee7a3e1fafe6e12bbc985f8
riphpro56-star <riphpro56@gmail.com>
2026-04-30 19:16:47 +0100

    Delete api directory

--- api/config.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    watchLink: process.env.WATCH_LINK || 'https://example.com/cpa-offer',
-    timezoneLabel: 'توقيت الجزائر'
-  });
-}
