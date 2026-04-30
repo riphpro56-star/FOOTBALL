53ff5bfc215675a3b196ea9f89d6e4906023aac1
riphpro56-star <riphpro56@gmail.com>
2026-04-30 16:24:59 +0100

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,1 +1,2 @@
+exports.handler=async function(){return{statusCode:200,headers:{'Content-Type':'application/json','Cache-Control':'public, max-age=300'},body:JSON.stringify({watchLink:process.env.WATCH_LINK||'https://example.com/cpa-offer',timezoneLabel:'توقيت الجزائر'})}};
 
